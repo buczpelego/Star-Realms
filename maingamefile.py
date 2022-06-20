789e86d109ba8f52033f3ed3e199da9063255575
buczpelego <buczpelego.git@wp.pl>
2022-06-20 18:14:35 +0200

    WERSJA STABILNA!, proces rozgrywki doprowadzony do końca, nie napotkano błędów

--- maingamefile.py
+++ maingamefile.py
@@ -1,3 +1,4 @@
+from numpy import sort
 from classobject import *
 from cards import *
 import os
@@ -19,6 +20,7 @@ explorer_deck.cardlist.extend([explorer1, explorer2, explorer3, explorer4, explo
 #R0zpoczecie gry
 while player1.health > 0 and cpu.health > 0:
     print("PLAYER TURN")
+    print('Player HP: ' + str(player1.health))
     print(player1.hand)
     print(player1.playarea)
     while True:
@@ -87,14 +89,17 @@ while player1.health > 0 and cpu.health > 0:
                                 continue
                     elif player_input > 6:
                         continue
-                    else: #kupowanie karty z marketu
+                    elif player_input > 0 and player_input <= len(market_place.cardlist): #kupowanie karty z marketu
                         chosen_card = market_place.cardlist[player_input - 1]
                         if player_total_gold < chosen_card.cost:
                             print('not enough gold to buy ' + str(chosen_card.name))
                         else:
                             player_total_gold -= chosen_card.cost
                             chosen_card.change_location(market_place, player1.discard)
-                            market_deck.cardlist[-1].change_location(market_deck, market_place)
+                            if len(market_deck.cardlist) > 0:
+                                market_deck.cardlist[-1].change_location(market_deck, market_place)
+                            else:
+                                pass
                             os.system('cls')
                             print('PLAYER TURN')
                             print(player1.playarea)
@@ -103,6 +108,8 @@ while player1.health > 0 and cpu.health > 0:
                             print(market_place)
                             print('If you can\'t buy any card form market, you can buy Explorer (BY PRESSING - 6) for 2 gold (Explorer gives 2 gold)')
                             continue
+                        continue
+                    else:
                         continue
                 else:
                     cpu.health -= player_total_damage #od tąd wchodzi zadawanie obrażeń przeciwnikowi
@@ -118,12 +125,14 @@ while player1.health > 0 and cpu.health > 0:
                     else: #warunek w przypadku końca kart w deku - przeniesienie i przetasowanie kart z discardu do decku
                         for i in range(len(player1.discard.cardlist)):
                             player1.discard.cardlist[-1].change_location(player1.discard, player1.deck)
-                            random.shuffle(player1.deck.cardlist)             
+                        random.shuffle(player1.deck.cardlist)             
                 break
         player_input = input('press any key')
         break
+    #TURA CPU
     os.system('cls')
     print('CPU Turn')
+    print('CPU HP: ' +str(cpu.health))
     for i in range(len(cpu.hand.cardlist)):
         cpu.hand.cardlist[-1].change_location(cpu.hand, cpu.playarea)
     cpu_total_gold = [card.gold for card in cpu.playarea.cardlist]
@@ -136,19 +145,76 @@ while player1.health > 0 and cpu.health > 0:
     print(market_place)
     player_input = input('press any key')
     os.system('cls')
-    print('CPU Turn')
+
     #mechanika kupna karty przez CPU:
-    while cpu_total_gold > 0:
+    if len(market_place.cardlist) > 0:   
         cards_avalible_to_buy = []
-        for card in market_place.cardlist:
+        for card in market_place.cardlist: #wybór spośród kart w markecie które meżna kupić za posiadaną ilość cpu_total_gold i dodanie do osobnej listy
             if card.cost <= cpu_total_gold:
                 cards_avalible_to_buy.append(card)
             else:
-                continue #! może być błąd
-        #opracować system wyboru najdroższej karty spośród możliwych do kupienia.
-    break
-        
+                pass #! może być błąd
+        if len(cards_avalible_to_buy) > 0:
+            sorted_cards_avalible_to_buy = sorted(cards_avalible_to_buy, key=lambda x: x.cost, reverse = True)    #posortowana lista kart mozliwych do kupienia przez CPU od najdroższej.
+            while cpu_total_gold > 0:
+                if sorted_cards_avalible_to_buy[0].cost <= cpu_total_gold: #jeśli pierwsza karta na liście kosztuje mniej lub tyle co cpu_total_gold to cpu ją kupuje, zmienia lokację na discard i dodaje kartę do marketu, pętla wraca do początku
+                    print('CPU Turn')
+                    print('CPU HP: ' +str(cpu.health))
+                    print(market_place)
+                    print('CPU gold left: ' + str(cpu_total_gold))
+                    chosen_card = sorted_cards_avalible_to_buy[0]
+                    market_place.cardlist[sorted_cards_avalible_to_buy.index(chosen_card)].change_location(market_place, cpu.discard)
+                    cpu_total_gold -= chosen_card.cost                      #po zakupie, cpu_total_gold zostaje zmniejszone o koszt kupionej karty
+                    if len(market_deck.cardlist) > 0: 
+                        market_deck.cardlist[-1].change_location(market_deck, market_place)
+                    else:
+                        break
+                    print('CPU bought ' + str(chosen_card))
+                    player_input = input('CPU gold left: ' + str(cpu_total_gold))
+                    os.system('cls')
+                    continue
+                elif sorted_cards_avalible_to_buy[0].cost > cpu_total_gold: #jeśli pierwsza karta na liście kosztuje więcej niż cpu total_gold, usuwa ją z listy i wraca do początku pętli
+                    sorted_cards_avalible_to_buy.remove(sorted_cards_avalible_to_buy[0])
+                    continue
+                elif len(sorted_cards_avalible_to_buy) == 2 and sorted_cards_avalible_to_buy[0].cost == 3 and sorted_cards_avalible_to_buy[1] == 1 and cpu_total_gold == 2: #warunek kupienia Odkrywcy przez CPU
+                    print('CPU Turn')
+                    print('CPU HP: ' +str(cpu.health))
+                    print(market_place)
+                    print('CPU gold left: ' + str(cpu_total_gold))
+                    chosen_card = explorer_deck[0]
+                    chosen_card.change_location(explorer_deck, cpu.discard)
+                    cpu_total_gold -= chosen_card.cost
+                    print('CPU bought ' +str(chosen_card))
+                    player_input = input('CPU gold left: ' + str(cpu_total_gold))
+                    os.system('cls')
+                    continue
+                elif len(sorted_cards_avalible_to_buy) == 2 and cpu_total_gold > 0: #wyzerowanie cpu_total_gold w przypadku braku możliwych kart do kupienia i posiadania niewykorzystanego złota
+                    cpu_total_gold = 0
+                    os.system('cls')
+                    continue
+                else: #przerwanie pętli w przypadku braku możliwych kart do kupienia i braku złota
+                    break
+        else:
+            pass            
+    else:
+        pass
     
+    player1.health -= cpu_total_damage
+    for i in range(len(cpu.playarea.cardlist)):
+        cpu.playarea.cardlist[-1].change_location(cpu.playarea, cpu.discard)
+    for i in range(5):
+        cpu.deck.cardlist[-1].change_location(cpu.deck, cpu.hand)
+        if len(cpu.deck.cardlist) > 0:
+            continue
+        else:
+            for i in range(len(cpu.discard.cardlist)):
+                cpu.discard.cardlist[-1].change_location(cpu.discard, cpu.deck)
+            random.shuffle(cpu.deck.cardlist)     
+
+if player1.health <= 0:
+    print('Player 1 lost')
+elif cpu.health <= 0:
+    print('Player 1 win')
 
 
-#Dodać wyświetlanie HP gracza, rozpocząć wprowadzanie tury CPU
+# WERSJA STABILNA!
